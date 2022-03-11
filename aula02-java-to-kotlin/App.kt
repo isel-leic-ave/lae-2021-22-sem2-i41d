92901766aa72d0e0cd8d59ed5c6cc5f69ae83baf
Miguel Gamboa <miguelgamboa@outlook.com>
2022-03-11 16:53:00 +0000

    aula 3

--- aula02-java-to-kotlin/App.kt
+++ aula02-java-to-kotlin/App.kt
@@ -1,4 +1,15 @@
+/**
+ * Se colocarmos em comentário a chamada a playWithPoint()
+ * - Compilar? Continuamos a precisar do Point.class da Metadata
+ * - Executar? Precisamos de Point.class? 
+ */
 fun main() {
+    playWithPoint()
+
+    println("Hello")
+}
+
+fun playWithPoint() {
     val p = Point(5, 7)
     print("Module of Point(5,7) = ")
     println(p.module)
--- aula02-java-to-kotlin/domain.kt
+++ aula02-java-to-kotlin/domain.kt
@@ -7,6 +7,7 @@ data class Account(val balance: Double, val id: String)
 data class Point(var x: Int, var y: Int) { // : Any(){ // Implict
     val module
         get() = sqrt((x*x + y*y).toDouble())
+    var z: Int = 0
 }
 
 /**
