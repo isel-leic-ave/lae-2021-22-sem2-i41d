9e123943eb69e9df98a286eee434419c19c6c99c
Miguel Gamboa <miguelgamboa@outlook.com>
2022-06-06 10:53:42 +0100

    Setup lesson 35

---
+++ aula35-closeable-and-cleaner/src/test/kotlin/pt/isel/CloseResourcesTest.kt
@@ -0,0 +1,18 @@
+package pt.isel
+
+import java.io.FileOutputStream
+import java.nio.file.FileSystemException
+import kotlin.io.path.Path
+import kotlin.io.path.deleteIfExists
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+private const val OUT = "out.txt"
+
+class CloseResourcesTest {
+
+    @Test
+    fun `test`() {
+    }
+}
